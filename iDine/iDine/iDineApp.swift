c3d8d4ab94386b273f566bd72a37ffac3255e969
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-02-23 10:44:18 +0100

    iDine : initial commit add resources

---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by HubertMac on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
