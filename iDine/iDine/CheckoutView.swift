2898b67f32d7260d4138d13575cc52e68cd46888
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-03-05 14:39:28 +0100

    Formating number strings

--- iDine/iDine/CheckoutView.swift
+++ iDine/iDine/CheckoutView.swift
@@ -19,6 +19,12 @@ struct CheckoutView: View {
     let tipAmounts = [10, 15, 20, 25, 0]
     @State private var tipAmount = 15
     
+    var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
+    
     var body: some View {
         Form{
             Section{
@@ -40,13 +46,13 @@ struct CheckoutView: View {
             Section("Add a tip?") {
                 Picker("Percentage", selection: $tipAmount) {
                     ForEach(tipAmounts, id: \.self) {
-                        Text("\($0)")
+                        Text("\($0)%")
                     }
                 }
                 .pickerStyle(.segmented)
             }
             
-            Section("Total: $100") {
+            Section("Total: \(totalPrice)") {
                 Button("Confirm Order") {
                     // place the order
                 }
